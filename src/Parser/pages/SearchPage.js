ecf275e2657d0caddc8bb34fdaddee760da1f9e3
Russkih Anatoly <titanofdarkness@gmail.com>
2019-06-12 18:40:49 +1000

    Navigation prototyping.

--- src/Parser/pages/SearchPage.js
+++ src/Parser/pages/SearchPage.js
@@ -1,10 +1,12 @@
 const logger = require('../../common/Logger')('src/Parser/pages/SearchPage.js');
-
+/*
+const LONG_DELAY_API = 2000;
 const TYPE_DELAY = 50;
+*/
 
 const NEXT_PAGE_SELECTOR = '.search-pagination__next-page-text';
-// const URL = 'https://apps.shopify.com/browse';
-const URL = 'https://apps.shopify.com/browse/store-design-popups-and-notifications?page=9&pricing=all&requirements=off&sort_by=popular';
+const URL = 'https://apps.shopify.com/browse';
+// const URL = 'https://apps.shopify.com/browse/store-design-popups-and-notifications?page=9&pricing=all&requirements=off&sort_by=popular';
 
 class SearchPage {
   constructor() {
@@ -23,24 +25,65 @@ class SearchPage {
     this.page = page;
   }
 
+  async clickOnNextSubCat(initialFlag = false) {
+    const { page } = this;
+    return page.evaluate((flag) => {
+      let isFount = flag;
+      let clicked = false;
+      const links = document.querySelectorAll('.search-filter-group__item-name');
+      for (const link of links) {
+        if (link.getAttribute('aria-current').toLowerCase() === 'true') {
+          isFount = true;
+        } else if (isFount) {
+          clicked = true;
+          link.click();
+          break;
+        }
+      }
+      if (!clicked) {
+        window.location.href = 'about:blank';
+      }
+    }, initialFlag);
+  }
+
+  async getCatAndSubCatOnPage() {
+    const { page } = this;
+    return page.evaluate(() => {
+      let result = null;
+      const links = document.querySelectorAll('.search-filter-group__item-name');
+      for (const link of links) {
+        if (link.getAttribute('aria-current').toLowerCase() === 'true') {
+          const subcategory = link.firstChild.nodeValue.trim();
+          const blockContainer = link.parentNode.parentNode.parentNode;
+          const category = blockContainer.querySelector('a').firstChild.nodeValue.trim();
+          result = { subcategory, category };
+          break;
+        }
+      }
+      return result;
+    });
+  }
+
+  setCatAndSubCat({ subcategory, category }) {
+    logger.debug(`Category: ${category}, Subcategory: ${subcategory}`);
+    this.category = category;
+    this.subcategory = subcategory;
+  }
+
   async checkCategories() {
     const { page } = this;
-    if (this.category === null) {
-      logger.debug('Setting first category');
-      this.category = await page.evaluate(() => document.querySelector('#CategoriesFilter a').firstChild.nodeValue.trim());
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('#CategoriesFilter a', { delay: TYPE_DELAY }),
-      ]);
+    const initialFlag = this.category === null;
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+      this.clickOnNextSubCat(initialFlag),
+    ]);
+    const result = await this.getCatAndSubCatOnPage();
+    if (result) {
+      this.setCatAndSubCat(result);
+    } else {
+      logger.debug('No category and subcategory on page. Parsing finished');
     }
-    if (this.subcategory === null) {
-      logger.debug('Setting first subcategory');
-      this.subcategory = await page.evaluate(() => document.querySelector('#CategoriesFilter ul a').firstChild.nodeValue.trim());
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('#CategoriesFilter ul a', { delay: TYPE_DELAY }),
-      ]);
-    }
+    return result;
   }
 
   async getProductsOnPage() {
@@ -99,58 +142,17 @@ class SearchPage {
       .contains('disabled'), NEXT_PAGE_SELECTOR);
   }
 
-  async clickOnNextSubCategory() {
-    const { page } = this;
-    return page.evaluate(() => {
-      let shouldClick = false;
-      let result = null;
-      const links = document.querySelectorAll('.search-filter-group__item-name');
-      for (const link of links) {
-        if (link.getAttribute('aria-current').toLowerCase() === 'true') {
-          shouldClick = true;
-        } else if (shouldClick) {
-          const subcategory = link.firstChild.nodeValue.trim();
-          result = { subcategory };
-          link.click();
-          break;
-        }
-      }
-      return result;
-    });
-  }
-
-  async goToNextSearchResult() {
-    const { page } = this;
-    logger.debug('go to next search result');
-    if (await this.isNextLinkOnPage()) {
-      logger.debug('Found Next link, clicking on it.');
-      return Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click(NEXT_PAGE_SELECTOR, { delay: TYPE_DELAY }),
-      ]);
-    }
-    const promise = page.waitForNavigation({ waitUntil: 'networkidle2' });
-    const nextCat = await this.clickOnNextSubCategory();
-    if (nextCat) {
-      this.subcategory = nextCat.subcategory;
-    } else {
-      await page.goto('about:blank', { waitUntil: 'networkidle2' });
-    }
-    return promise;
-  }
-
   async Parse() {
     const url = await this.page.url();
     logger.debug('Parsing %s', url);
     await this.checkCategories();
     const products = await this.getProductsOnPage();
+    logger.debug('Found %d products', products.length);
     const source = {
       Category: this.category,
       Subcategory: this.subcategory,
     };
-    const productsComplete = products.map(product => Object.assign(product, source));
-    await this.goToNextSearchResult();
-    return productsComplete;
+    return products.map(product => Object.assign(product, source));
   }
 }
 
