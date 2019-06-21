cbb966c9ab734370fb2f2ec87c873da2ca9a76b2
Russkih Anatoly <titanofdarkness@gmail.com>
2019-06-21 14:44:35 +1000

    Minor fix ref

--- src/db/models/Review.js
+++ src/db/models/Review.js
@@ -3,13 +3,14 @@ const logger = require('../../common/Logger')('src/db/models/Review.js');
 
 module.exports = (mongoose) => {
   const { Schema } = mongoose;
+  const productsName = `productsScrapred${dateFormat(new Date(), 'yyyymmdd')}`;
   const reviewSchema = new Schema({
     Id: Number,
     Rating: Number,
     Header: String,
     Description: String,
     ReviewDate: Date,
-    ProductId: { type: Schema.Types.ObjectId, ref: 'ProductPage' },
+    ProductId: { type: Schema.Types.ObjectId, ref: productsName },
   });
 
   const collectionName = `reviewsScrapred${dateFormat(new Date(), 'yyyymmdd')}`;
